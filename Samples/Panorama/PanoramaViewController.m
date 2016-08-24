fd6f50a8bf07558b491e4538c628d0a0b7aef7a7
Scott Mason <scott@madetech.co.uk>
2016-08-24 14:51:17 +0100

    Add multiple floor images and allow switching between them

--- Samples/Panorama/PanoramaViewController.m
+++ Samples/Panorama/PanoramaViewController.m
@@ -4,45 +4,54 @@
 int officeFloor = 0;
 
 @interface PanoramaViewController ()<GVRWidgetViewDelegate>
-- (void)updatePanoView;
+- (void)updateFloorView;
+- (Boolean)transitionFloor:(float) pitchAngle;
 @end
 
 @implementation PanoramaViewController {
   GVRPanoramaView *_panoView;
 }
 
-- (void)updatePanoView {
+- (void)updateFloorView {
     [_panoView loadImage:[UIImage imageNamed:[self getFloorImage:officeFloor]]
                   ofType:kGVRPanoramaImageTypeMono];
-}
-
-- (NSString *)getFloorImage:(int)floorNumber {
-    NSArray *floorImages = @[@"k+1.jpeg", @"andes.jpg", @"k+1.jpeg", @"andes.jpg"];
-    return floorImages[floorNumber];
-
 }
 
 - (void)loadView {
     _panoView = [[GVRPanoramaView alloc] init];
     _panoView.delegate = self;
     _panoView.displayMode = kGVRWidgetDisplayModeFullscreenVR;
-    [self updatePanoView];
+    [self updateFloorView];
     self.view = _panoView;
 }
 
-- (void)widgetViewDidTap:(GVRWidgetView *)widgetView {
+- (NSString *)getFloorImage:(int)floorNumber {
+    NSArray *floorImages = @[@"k+0.jpg", @"k+1.jpg", @"k+2.jpg", @"k+3.jpg"];
+    return floorImages[floorNumber];
+}
+
+- (Boolean)transitionFloor:(float) pitchAngle {
+    Boolean changed = false;
     if (_panoView.headRotation.pitch >= 40) {
-      if (officeFloor < 3) {
-        officeFloor++;
-      }
+        if (officeFloor < 3) {
+            officeFloor++;
+            changed = true;
+        }
     } else if (_panoView.headRotation.pitch <= -40) {
-      if (officeFloor > 0) {
-        officeFloor--;
-      }
+        if (officeFloor > 0) {
+            officeFloor--;
+            changed = true;
+        }
+    }
+    return changed;
+}
+
+- (void)widgetViewDidTap:(GVRWidgetView *)widgetView {
+    if ([self transitionFloor:_panoView.headRotation.pitch]) {
+      [self updateFloorView];
     }
     
     NSLog(@(_panoView.headRotation.pitch).stringValue);
     NSLog(@(_panoView.headRotation.yaw).stringValue);
-    [self updatePanoView];
 }
 @end
