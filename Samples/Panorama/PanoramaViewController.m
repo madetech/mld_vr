5b2c0c3555374c7e32a8dc60055ee538d6310ccf
Scott Mason <scott@madetech.co.uk>
2016-08-24 15:43:59 +0100

    Add easter egg faces

--- Samples/Panorama/PanoramaViewController.m
+++ Samples/Panorama/PanoramaViewController.m
@@ -2,9 +2,11 @@
 #import "GVRPanoramaView.h"
 
 int officeFloor = 0;
+NSString * easterEgg = NULL;
 
 @interface PanoramaViewController ()<GVRWidgetViewDelegate>
 - (void)updateFloorView;
+- (void)updateImageView:(NSString *)image;
 - (Boolean)transitionFloor:(float) pitchAngle;
 @end
 
@@ -14,6 +16,11 @@ int officeFloor = 0;
 
 - (void)updateFloorView {
     [_panoView loadImage:[UIImage imageNamed:[self getFloorImage:officeFloor]]
+                  ofType:kGVRPanoramaImageTypeMono];
+}
+
+- (void)updateImageView:(NSString *)image {
+    [_panoView loadImage:[UIImage imageNamed:image]
                   ofType:kGVRPanoramaImageTypeMono];
 }
 
@@ -46,11 +53,41 @@ int officeFloor = 0;
     return changed;
 }
 
-- (void)widgetViewDidTap:(GVRWidgetView *)widgetView {
-    if ([self transitionFloor:_panoView.headRotation.pitch]) {
-      [self updateFloorView];
+- (Boolean)easterEggTransition:(GVRHeadRotation) headRotation {
+    if (easterEgg != NULL) {
+        easterEgg = NULL;
+        [self updateFloorView];
+        return false;
     }
     
+    Boolean seb = ABS(headRotation.pitch) <= 5 && (ABS(headRotation.yaw + 122)) <= 5 && officeFloor == 1;
+    Boolean luke = ABS(headRotation.pitch) <= 5 && (ABS(headRotation.yaw + 146)) <= 5 && officeFloor == 1;
+    Boolean scott = ABS(headRotation.pitch - 6) <= 5 && (ABS(headRotation.yaw + 155)) <= 5 && officeFloor == 2;
+    Boolean k2Door = ABS(headRotation.pitch - 10) <= 10 && (ABS(headRotation.yaw - 164)) <= 5 && officeFloor == 2;
+    Boolean chris = ABS(headRotation.pitch + 8) <= 5 && (ABS(headRotation.yaw - 167)) <= 5 && officeFloor == 3;
+    
+    if (seb) {
+      easterEgg = @"seb.jpg";
+    } else if (luke) {
+      easterEgg = @"luke.jpg";
+    } else if (chris) {
+      easterEgg = @"chris.jpg";
+    } else if (scott) {
+      easterEgg = @"scott.jpg";
+    } else if (k2Door) {
+      easterEgg = @"k+2door.jpg";
+    }
+
+    return easterEgg != NULL;
+}
+
+- (void)widgetViewDidTap:(GVRWidgetView *)widgetView {
+    if (easterEgg == NULL && [self transitionFloor:_panoView.headRotation.pitch]) {
+      [self updateFloorView];
+    } else if ([self easterEggTransition:_panoView.headRotation]) {
+      [self updateImageView:easterEgg];
+    }
+
     NSLog(@(_panoView.headRotation.pitch).stringValue);
     NSLog(@(_panoView.headRotation.yaw).stringValue);
 }
