bee47562c1b7644695b3e23d9c1a067c991ed79c
Scott Mason <scott@madetech.co.uk>
2016-08-24 14:00:30 +0100

    Refactor floor setting logic

--- Samples/Panorama/PanoramaViewController.m
+++ Samples/Panorama/PanoramaViewController.m
@@ -1,36 +1,56 @@
 #import "PanoramaViewController.h"
 #import "GVRPanoramaView.h"
 
+int officeFloor = 0;
+
 @interface PanoramaViewController ()<GVRWidgetViewDelegate>
-- (void)setPanoView:(NSString *)imageName;
+- (void)updatePanoView;
 @end
 
 @implementation PanoramaViewController {
   GVRPanoramaView *_panoView;
 }
 
-- (void)setPanoView:(NSString *)imageName {
-    _panoView = [[GVRPanoramaView alloc] init];
-    _panoView.delegate = self;
-    _panoView.displayMode = kGVRWidgetDisplayModeFullscreenVR;
-    [_panoView loadImage:[UIImage imageNamed:imageName]
+- (void)updatePanoView {
+    [_panoView loadImage:[UIImage imageNamed:[self getFloorImage:officeFloor]]
                   ofType:kGVRPanoramaImageTypeMono];
-    self.view = _panoView;
+}
+
+- (NSString *)getFloorImage:(int)floorNumber {
+    switch (floorNumber) {
+        case 3:
+            return @"andes.jpg";
+        case 2:
+            return @"k+1.jpeg";
+        case 1:
+            return @"andes.jpg";
+        case 0:
+        default:
+            return @"k+1.jpeg";
+    }
 }
 
 - (void)loadView {
-    [self setPanoView:@"k+1.jpeg"];
+    _panoView = [[GVRPanoramaView alloc] init];
+    _panoView.delegate = self;
+    _panoView.displayMode = kGVRWidgetDisplayModeFullscreenVR;
+    [self updatePanoView];
+    self.view = _panoView;
 }
 
 - (void)widgetViewDidTap:(GVRWidgetView *)widgetView {
     if (_panoView.headRotation.pitch >= 40) {
-        printf("GO UP");
-        [_panoView loadImage:[UIImage imageNamed:@"andes.jpg"]
-                      ofType:kGVRPanoramaImageTypeMono];
+      if (officeFloor < 3) {
+        officeFloor++;
+      }
     } else if (_panoView.headRotation.pitch <= -40) {
-        printf("GO DOWN");
-        [_panoView loadImage:[UIImage imageNamed:@"andes.jpg"]
-                      ofType:kGVRPanoramaImageTypeMono];
+      if (officeFloor > 0) {
+        officeFloor--;
+      }
     }
+    
+    NSLog(@(_panoView.headRotation.pitch).stringValue);
+    NSLog(@(_panoView.headRotation.yaw).stringValue);
+    [self updatePanoView];
 }
 @end
